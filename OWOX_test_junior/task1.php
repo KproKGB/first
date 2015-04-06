740ca5d60307d5636afc2c92ff884a920ee75342
Kpro KGB <pchelper13@gmail.com>
2015-04-06 17:08:08 +0300

    add test

---
+++ OWOX_test_junior/task1.php
@@ -0,0 +1,20 @@
+<?php
+/*function searchInArray($a, $b)
+{
+    foreach ($a as $value) {
+        if ($value == $b) {
+            return true;
+        }
+    }
+    return false;
+}*/
+
+
+function searchInArray($a, $b){
+
+}
+$arr = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+$num = rand(1, 20);
+
+echo intval(searchInArray($arr, $num));
+?>
--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 <a href="cookie/index.php">Cookie</a><br>
 <a href="session/index.php">Session</a><br>
 <a href="test/index.php">Test</a><br>
+<a href="OWOX_test_junior/index.php">OWOX</a><br>
 </body>
 </html>
