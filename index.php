450152ca1cc080923972b14ee90baefb88d740d5
Kpro KGB <pchelper13@gmail.com>
2015-04-06 07:23:41 +0300

    v1.01

---
+++ index.php
@@ -0,0 +1,11 @@
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>Main</title>
+</head>
+<body>
+<a href="cookie/index.php">Cookie</a><br>
+<a href="session/index.php">Session</a><br>
+<a href="test/index.php">Test</a><br>
+</body>
+</html>
