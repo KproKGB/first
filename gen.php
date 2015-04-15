a305a98223c78fb897db337f75babe47f3b6f520
Kpro KGB <pchelper13@gmail.com>
2015-04-15 17:09:54 +0300

    GenPass V1.02

--- gen.php
+++ gen.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 include 'inc/data.inc.php';
+define ('PATH_LOG', 'path.log');
+include 'inc/log.inc.php';
 ?>
 <html>
 <head>
@@ -17,13 +19,15 @@ include 'inc/data.inc.php';
                         <h2>Генератор паролей</h2>
                         <form action='<?php echo $_SERVER['REQUEST_URI']?>' method='post'>
                             Варианты:
-                            <input type='checkbox' checked name="zagl">Заглавные буквы
-                            <input type='checkbox' checked name="propis">Маленькие буквы
-                            <input type='checkbox' checked name="chisl">Цифры</br></br>
+                            <input type='checkbox' <?php if(isset($_POST['zagl'])) echo 'checked';?> name="zagl">Заглавные буквы
+                            <input type='checkbox' <?php if(isset($_POST['propis'])) echo 'checked';?> name="propis">Маленькие буквы
+                            <input type='checkbox' <?php if(isset($_POST['chisl'])) echo 'checked';?> name="chisl">Цифры</br></br>
                             Длина пароля:
-                            <input name="len" placeholder="Длинна пароля" value="8" autofocus required pattern="(^[0-9]+$)"><br><br>
+                            <input name="len" placeholder="Длинна пароля" value="<?php if(isset($_POST['len'])) echo $_POST['len'];?>"
+                                   autofocus required pattern="(^[0-9]+$)"><br><br>
                             Количество паролей:
-                            <input name="kol" placeholder="Количество паролей" value="1" autofocus required pattern="(^[0-9]+$)"><br><br>
+                            <input name="kol" placeholder="Количество паролей" value="<?php if(isset($_POST['kol'])) echo $_POST['kol'];?>"
+                                   autofocus required pattern="(^[0-9]+$)"><br><br>
                             <input type="submit" value="Создать пароль">
                         </form>
                     </td>
