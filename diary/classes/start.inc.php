366f0c3edff1fa04442e93ff866355a97f7e39ae
KproKGB <pchelper13@gmail.com>
2015-05-26 18:04:10 +0300

    test

--- diary/classes/start.inc.php
+++ diary/classes/start.inc.php
@@ -1,5 +1,31 @@
 <?php
-function clearStr ($data){
-    return trim(htmlspecialchars($data));
+class newDiary
+{
+    protected $_link;
+    const DBHOST = 'localhost';
+    const DBLOGIN = 'root';
+    const DBPASS = '1811';
+
+    function dbConnect(){
+        $this->_link = mysqli_connect(self::DBHOST, self::DBLOGIN, self::DBPASS);
+        if (!$this->_link)
+            echo 'Ошибка соединения: ' . mysqli_connect_errno();
+        else
+            echo "Успешное подключение<br><br>";
+
+    }
+    function dbCreate(){
+        $this->_link = mysqli_connect(self::DBHOST, self::DBLOGIN, self::DBPASS);
+        $_sql = "CREATE TABLE IF NOT EXISTS  users (id int NOT NULL AUTO_INCREMENT,
+                            login varchar(20) NOT NULL,
+                            password VARCHAR(235) NOT NULL,
+                            email VARCHAR(235) NOT NULL,
+                            acctype VARCHAR(235) NOT NULL,
+                            date TIMESTAMP,
+                            PRIMARY KEY(id));";
+
+        mysqli_query($this->_link, $_sql) or die ('Error');
+    }
+
 }
 ?>
--- diary/index.php
+++ diary/index.php
@@ -1,27 +1,7 @@
 <?php
-session_start();
 header('Content-Type: text/html; charset=utf-8');
 require 'classes/start.inc.php';
-$a='Вася';
-$b='c8837b23ff8aaa8a2dde915473ce0991';
-
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    $_SESSION['name'] = clearStr($_POST['name']);
-    $_SESSION['pswd'] = clearStr($_POST['pswd']);
-}
-if(!isset ($_SESSION['name']) and !isset ($_SESSION['pswd'])){
-    include "classes/loginForm.html";
-    }
-else {
-    if ($_SESSION['name'] == $a and md5($_SESSION['pswd']) == $b) {
-        echo "Вы авторизовались, как $a!";
-        include "classes/logout.html";
-    }
-    else{
-       echo $_SESSION['name'];
-       echo $_SESSION['pswd'];
-    }
-}
-if (isset($_POST['exit']))
-    session_destroy();
+$con = new newDiary();
+$con->dbConnect();
+$con->dbCreate();
 ?>
