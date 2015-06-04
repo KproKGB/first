2b267f6850c01abe295729c971e44a43e91f1e57
KproKGB <pchelper13@gmail.com>
2015-06-04 17:49:50 +0300

    test

--- diary/index.php
+++ diary/index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 header('Content-Type: text/html; charset=utf-8');
+include 'inc/parse_ini.inc.php';
 include 'inc/headers.inc.php';
 include 'classes/login.class.php';
-include 'inc/parse_ini.inc.php';
 $loginUsr = new loginUsers();
 $loginUsr->dbConnect($host, $user, $pass, $base);
-debug_print_backtrace();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_SESSION['lname'] = trim(htmlspecialchars($_POST['lname']));
     $_SESSION['lpswd'] = $_POST['lpswd'];
@@ -17,7 +17,6 @@ if (isset($_SESSION['lname']) and !empty($_SESSION['lname'])) {
     if($loginUsr->loginUsr($_SESSION['lname'], $_SESSION['lpswd'])){
         include 'inc/form/start.html';
     }
-
 }
 else {
     include 'inc/form/loginForm.html';
