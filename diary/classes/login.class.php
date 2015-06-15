c622ed510d3a8373495c0b038b593c9a6ec601f6
KproKGB <pchelper13@gmail.com>
2015-06-15 17:09:59 +0300

    1

--- diary/classes/login.class.php
+++ diary/classes/login.class.php
@@ -4,24 +4,19 @@ class loginUsers{
     function dbConnect($dbhost, $dbuser, $dbpass, $dbbase){
         $this->_link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbbase);
     }
+
     function loginUsr($login, $password){
         $login = mysqli_real_escape_string($this->_link, trim(htmlspecialchars($login)));
         $password = mysqli_real_escape_string($this->_link, md5(trim(htmlspecialchars($password))));
         $sql = "SELECT login FROM users WHERE login='$login' AND pass='$password'";
         $query = mysqli_query($this->_link, $sql);
         $count = mysqli_num_rows($query);
-        if ($count) {
-            echo "Вы авторизовались, как $login!";
-        } else {
-           echo "Ошибка авторизации!!!";
+        if (!$count) {
+            echo "Ошибка авторизации!!!";
             session_destroy();
         }
-
         return !!$count;
-
     }
-
-
 
     function dbClose(){
         mysqli_close($this->_link);
