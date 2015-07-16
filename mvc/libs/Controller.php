3a36a3bdb43fc84d178494e63bc20652dccbff19
Kpro KGB <pchelper13@gmail.com>
2015-07-16 22:02:06 +0300

    header

--- mvc/libs/Controller.php
+++ mvc/libs/Controller.php
@@ -1,6 +1,7 @@
 <?php
 class Controller {
     public function __construct() {
+        header('Content-Type: text/html; charset=utf-8');
         echo "Это главный контроллер<br/>";
         $this->view = new View();
     }
