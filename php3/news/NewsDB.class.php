5bc6a2e985d0b79f70763384421340f15702f2cc
Kpro KGB <pchelper13@gmail.com>
2015-08-19 08:02:20 +0300

    19/08v1

--- php3/news/NewsDB.class.php
+++ php3/news/NewsDB.class.php
@@ -1,1 +1,35 @@
 <?php
+require_once "INewsDB.class.php";
+class NewsDB implements INewsDB {
+    const DB_NAME = "news.db";
+    private $_db;
+
+    function __construct() {
+        $this->_db = new SQLite3(self::DB_NAME);
+    }
+
+    function __get($name) {
+        if($name == "db") {
+            return $this->_db;
+        }
+        throw new Exception ("Unknown property!");
+    }
+
+    function saveNews($title, $category, $description, $source) {
+
+    }
+
+    function getNews() {
+
+    }
+
+    function deleteNews($id){
+
+    }
+
+    function __destruct() {
+        unset($this->_db);
+    }
+}
+
+$news = new NewsDB();
