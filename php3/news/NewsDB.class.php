1afb5724a054e9f5a2d9c35c4f469e6beee08b7d
Kpro KGB <pchelper13@gmail.com>
2015-08-20 07:56:54 +0300

    20/08v1

--- php3/news/NewsDB.class.php
+++ php3/news/NewsDB.class.php
@@ -6,6 +6,29 @@ class NewsDB implements INewsDB {
 
     function __construct() {
         $this->_db = new SQLite3(self::DB_NAME);
+        if(is_file(self::DB_NAME) and filesize(self::DB_NAME) == 0) {
+            $sql = "CREATE TABLE msgs(
+	                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+	                  title TEXT,
+	                  category INTEGER,
+	                  description TEXT,
+	                  source TEXT,
+	                  datetime INTEGER
+                    )";
+            $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+
+            $sql = "CREATE TABLE category(
+	                  id INTEGER,
+	                  name TEXT
+                    )";
+            $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+
+            $sql = "INSERT INTO category(id, name)
+                        SELECT 1 as id, 'Политика' as name
+                        UNION SELECT 2 as id, 'Культура' as name
+                        UNION SELECT 3 as id, 'Спорт' as name ";
+            $this->_db->exec($sql) or die($this->_db->lastErrorMsg());
+        }
     }
 
     function __get($name) {
@@ -16,7 +39,10 @@ class NewsDB implements INewsDB {
     }
 
     function saveNews($title, $category, $description, $source) {
-
+        $dt = time();
+        $sql = "INSERT INTO msgs (title, category, description, source, datetime)
+                        VALUES ('$title', $category, '$description', '$source', $dt)";
+        return $this->_db->exec($sql);
     }
 
     function getNews() {
@@ -30,6 +56,12 @@ class NewsDB implements INewsDB {
     function __destruct() {
         unset($this->_db);
     }
-}
 
-$news = new NewsDB();
+    function clearStr($data) {
+        $data = strip_tags($data);
+        return $this->_db->escapeString($data);
+    }
+    function clearInt($data) {
+        return abs((int)$data);
+    }
+}
