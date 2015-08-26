128ff7cc27fb9a8dd44fb3de20eb0bfb60e38ad6
KproKGB <pchelper13@gmail.com>
2015-08-26 18:05:58 +0300

    26/08v2

--- php2/eshop/admin/orders.php
+++ php2/eshop/admin/orders.php
@@ -3,8 +3,8 @@ require "secure/session.inc.php";
 require "../inc/lib.inc.php";
 require "../inc/config.inc.php";
 $orders = getOrders();
-
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,14 +13,17 @@ $orders = getOrders();
 </head>
 <body>
 <h1>Поступившие заказы:</h1>
+
 <?php
 foreach($orders as $order) {
 $dt = (int)$order['date'];
 $dt = date("d-m-Y H:i", $dt);
-
+$i = 1;
+$sum = 0;
 ?>
+
 <hr>
-<h2>Заказ номер: <?= $order['orderid']?></h2>
+<h2>Заказ номер: <?= $order['orderid'] ?></h2>
 
 <p><b>Заказчик</b>: <?= $order['name'] ?></p>
 
@@ -30,7 +33,7 @@ $dt = date("d-m-Y H:i", $dt);
 
 <p><b>Адрес доставки</b>: <?= $order['address'] ?></p>
 
-<p><b>Дата размещения заказа</b>: <?= $dt?></p>
+<p><b>Дата размещения заказа</b>: <?= $dt ?></p>
 
 <h3>Купленные товары:</h3>
 <table border="1" cellpadding="5" cellspacing="0" width="90%">
@@ -42,12 +45,31 @@ $dt = date("d-m-Y H:i", $dt);
         <th>Цена, руб.</th>
         <th>Количество</th>
     </tr>
+
     <?php
-    }
-?>
+        foreach($order['goods'] as $item) {
+            $sum += $item['price'] * $item['quantity'];
+    ?>
+
+    <tr>
+        <td><?= $i++ ?></td>
+        <td><?= $item['title'] ?></td>
+        <td><?= $item['author'] ?></td>
+        <td><?= $item['pubyear'] ?></td>
+        <td><?= $item['price'] ?></td>
+        <td><?= $item['quantity'] ?></td>
+    </tr>
+
+    <?php
+        }
+    ?>
 
 </table>
-<p>Всего товаров в заказе на сумму: руб.</p>
+<p>Всего товаров в заказе на сумму: <?= $sum?>грн.</p>
+
+<?php
+}
+?>
 
 </body>
 </html>
--- php2/eshop/admin/secure/login.php
+++ php2/eshop/admin/secure/login.php
@@ -1,6 +1,22 @@
-<?
+<?php
+session_start();
+header("HTTP/1.0 401 Unauthorized");
+require_once "secure.inc.php";
 $title = 'Авторизация';
 $login  = '';
+if($_SERVER['REQUEST_METHOD']=="POST") {
+	$login = trim(htmlspecialchars($_POST['login']));
+	$pw = trim(htmlspecialchars($_POST['pw']));
+	$ref = trim(strip_tags($_GET['ref']));
+	if(!$ref) {
+		$ref = '/eshop/admin/';
+	}
+	if($login and $pw) {
+		if($result = userExists($login)) {
+
+		}
+	}
+}
 ?>
 <!DOCTYPE HTML>
 <html>
