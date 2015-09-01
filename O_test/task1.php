5280bfe9d06fa6387b489f09f6505b00ff5bfa63
KproKGB <pchelper13@gmail.com>
2015-09-01 18:23:43 +0300

    1/09v2

--- O_test/task1.php
+++ O_test/task1.php
@@ -4,18 +4,30 @@
 есть ли в массиве искомый элемент и вернет TRUE или FALSE.
 Не стоит использовать in_array или array_search */
 
-function searchInArray($a, $b) {
-    if (($b > $a[count($a)-1]) or ($b < $a[0])) {
-        return false;
-    }
-    return true;
-}
-
 /*function searchInArray($a, $b) {
-    foreach ($a as $key => $value) {
+    foreach ($a as $value) {
         if ($value == $b) {
             return true;
         }
-        return false;
     }
+    return false;
 }*/
+
+$a = array(2, 3, 5, 7, 8, 9, 10);
+$b = 7;
+
+function searchInArray($a, $b) {
+    $s = $a[0];
+    $e = count($a)-1;
+    while($s < $e) {
+        $m = intval(($s + $e) / 2);
+        if ($a[$m] < $b) {
+            $s = $a[$m] +1;
+        } else {
+            $e = $m;
+        }
+    }
+    return $a[$e]==$b;
+}
+
+var_dump(searchInArray($a, $b));
