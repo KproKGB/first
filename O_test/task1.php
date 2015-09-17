a2a607ce987dc3288e80daa1e5051aef5856327b
KproKGB <pchelper13@gmail.com>
2015-09-17 17:54:19 +0300

    17/09v2

--- O_test/task1.php
+++ O_test/task1.php
@@ -5,15 +5,17 @@
 Не стоит использовать in_array или array_search */
 
 $a = array(102, 103, 105, 107, 108, 109, 110);
-$b = 106;
+$b = 107;
 
 function searchInArray($a, $b) {
     $s = 0;
     $e = count($a)-1;
     while($s < $e) {
         $m = intval(($s + $e) / 2);
-        if ($a[$m] < $b) {
-            $s = $a[$m] +1;
+        if($a[$m] == $b) {
+            return $a[$m] == $b;
+        } elseif ($a[$m] < $b) {
+            $s = $m + 1;
         } else {
             $e = $m;
         }
