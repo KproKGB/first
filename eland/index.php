d3ed60daca974639fcc1f205df5f42b100202c81
Kpro KGB <pchelper13@gmail.com>
2015-09-22 07:45:41 +0300

    22/09v1

--- eland/index.php
+++ eland/index.php
@@ -9,56 +9,63 @@ $showHere = "Electro-land";
     <link rel="stylesheet" href="inc/style.css" />
 </head>
 <body>
-<!--Логотип-->
-<div id="header">
-    <img src="img/logo.png" width="489" height="85" class="logo" />
+<div id="body">
+
+    <!--Корзина-->
+    <div id="basket">Корзина</div>
+    <!--Корзина-->
+
+
+    <!--Логотип-->
+    <div id="header">
+        <img src="img/logo.png" width="489" height="85" class="logo" />
+    </div>
+    <!--Логотип-->
+
+
+    <!--Верхнее меню-->
+    <div id="topmenu">
+        <ul>
+            <li><a href="http://first/eland/index.php">ГЛАВНАЯ</a></li>
+            <li><a href="../php2/eshop/catalog.php">МАГАЗИН</a></li>
+            <li><a href="/eland">ДОСТАВКА И ОПЛАТА</a></li>
+            <li><a href="../php2/eshop/admin/index.php">ИНФОРМАЦИЯ</a></li>
+        </ul>
+    </div>
+    <!--Верхнее меню-->
+
+
+
+    <!--Показываем расположение на сайте (по меню)-->
+    <div id="showHere">
+        <span>Вы находитесь:</span>
+        <span><?= $showHere?></span>
+    </div>
+    <!--Показываем расположение на сайте (по меню)-->
+
+
+    <!--Правое меню-->
+    <div id="right_menu"></div>
+    <!--Правое меню-->
+
+
+    <!--Блок основного контента-->
+    <div id="main">
+        <br />
+        ВСЕМ ПРИВЕТ!!!
+        <br /><br />
+        ОСНОВНОЙ КОНТЕНТ
+        <br /><br />
+    </div>
+    <!--Блок основного контента-->
+
+
+    <!--Футер-->
+    <div id="footer">
+       &copy; 05-2015 - <?= date('m-Y')?> Electro-land.dp.ua | Designed by <a href="#"> Kpro</a>
+    </div>
+    <!--Футер-->
+
 </div>
-<!--Логотип-->
-
-<!--Корзина-->
-<div id="basket"></div>
-<!--Корзина-->
-
-<!--Верхнее меню-->
-<div id="topmenu">
-    <ul>
-        <li><a href="http://first/eland/index.php">ГЛАВНАЯ</a></li>
-        <li><a href="../php2/eshop/catalog.php">МАГАЗИН</a></li>
-        <li><a href="">ДОСТАВКА И ОПЛАТА</a></li>
-        <li><a href="../php2/eshop/admin/index.php">ИНФОРМАЦИЯ</a></li>
-    </ul>
-</div>
-<!--Верхнее меню-->
-
-
-
-<!--Показываем расположение на сайте (по меню)-->
-<div id="showHere">
-    <span>Вы находитесь:</span>
-    <span><?= $showHere?></span>
-</div>
-<!--Показываем расположение на сайте (по меню)-->
-
-<!--Правое меню-->
-<div id="right_menu"></div>
-<!--Правое меню-->
-
-<!--Блок основного контента-->
-<div id="main">
-    <br />
-    ВСЕМ ПРИВЕТ!!!
-    <br /><br />
-    ОСНОВНОЙ КОНТЕНТ
-    <br /><br />
-</div>
-<!--Блок основного контента-->
-
-<!--Футер-->
-<div id="footer">
-   &copy; 05-2015 - <?= date('m-Y')?> Electro-land.dp.ua | Designed by
-    <a href="#"> Kpro</a>
-</div>
-<!--Футер-->
-
 </body>
 </html>
