5f2365651600e0afed29188030dc60fe9b925248
Kpro KGB <pchelper13@gmail.com>
2015-10-18 17:16:49 +0300

    18/10v1

--- notes/index.php
+++ notes/index.php
@@ -1,13 +1,27 @@
 <html>
 <head>
     <title>Шпаргалка по GitHub</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="inc/style.css">
     <meta charset="utf-8">
 </head>
 <body>
-
 <h2>Цикл разработки через <a href="https://github.com/">github</a></h2>
+
+<nav id="menu">
+    <ul>
+        <li><a href="#1">Создаем новый ключ SSH</a></li>
+        <li><a href="#2">Настройка параметров пользователя</a></li>
+        <li><a href="#3">Разработка</a></li>
+        <li><a href="#4">Тестирование</a></li>
+        <li><a href="#5">Релиз</a></li>
+        <li><a href="#6">Команды GIT</a></li>
+    </ul>
+</nav>
+
+
 <hr>
+<div class="create">
+    <selection id="1">
 <h3>Создаем новый ключ SSH</h3>
 <ol>
     <li><p>ssh-keygen -t rsa -b 4096 -C "your_email@example.com"</p></li>
@@ -35,19 +49,21 @@
 
     <li><p>Проверка соединения:<br><br>
         <code>ssh -T git@github.com</code></p></li>
-</ol>
-<br>
+</ol></selection>
+<br></div>
 <hr>
-
+<div class="setting"><selection id="2">
 <h3>Настройка параметров пользователя</h3>
 
 <p>Нужно настроить параметры для истории и удобства</p>
 <code>$ git config --global user.name "Name"<br>
 $ git config --global user.email "email@email.com"<br>
 $ git config --global color.ui true</code><br><br>
-
+</selection>
+</div>
 <hr>
-<ol>
+<div class="dev">
+<ol><selection id="3">
 <h3>Разработка</h3>
 
 <p>Существует 2 основных ветки: <b>master</b> и <b>dev</b>.</p>
@@ -125,8 +141,9 @@ git commit -m "#1234 changing backbone model url"</code>
 <li>
     <p>Отлично! Все изменения сделаны, и кто-то написал комментарий «merge it» в Pull Request. Пора нажимать кнопку
     <b>Merge pull request</b>, чтобы влить изменения <b>1234-bug-login<</b> в <b>dev</b> ветку.</p>
-</li>
+</li></selection>
 <hr>
+    <selection id="4">
     <h3>Тестирование</h3>
 <li>
     <p>Как только <b>1234-bug-login</b> попадает в <b>dev</b>, Jenkins (система непрерывной интеграции) автоматически
@@ -137,9 +154,9 @@ git commit -m "#1234 changing backbone model url"</code>
 <li>
     <p>Если Pull Request вносит много изменений, разработчик может при помощи Jenkins загрузить свою ветку на
         <b>qa сервер</b> для проверки тестерами.</p>
-</li>
+</li></selection>
 <hr>
-
+<selection id="5">
     <h3>Релиз</h3>
 <li>
         <p>Перед обновлением production сервера необходимо влить <b>dev</b> ветку в <b>master</b>. Для этого мы создаем <b>Pull Request</b>
@@ -164,9 +181,11 @@ git commit -m "#1234 changing backbone model url"</code>
     git pull origin dev<br><br>
     git merge master<br><br>
     git push origin dev</code>
-</ol>
+</selection>
+</ol></div>
 <hr>
-
+<div class="notes">
+    <selection id="6">
 <h3>Команды GIT</h3>
 <ol>
     <li>Для инициализации локального репозитория необходимо выполнить:<br>
@@ -225,7 +244,7 @@ git commit -m "#1234 changing backbone model url"</code>
         Возвращение на рабочуюю версию файла<br>
         <code>git checkout --ours &lt;file&gt;</code>
     </li>
-</ol>
-
+</ol></selection>
+</div>
 </body>
 </html>
