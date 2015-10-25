f53deb5a97dda841945fb38e8ba3dd74d6cdbb62
KproKGB <pchelper13@gmail.com>
2015-10-25 16:53:50 +0200

    pb start

---
+++ pb/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>PB test</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<nav class="header">
+    <img src="logo_big2.png">
+    <ul>
+        <li><a href="">О ГРУППЕ</a></li>
+        <li><a href="">КАРЬЕРА</a></li>
+        <li><a href="">ВОЙТИ</a></li>
+    </ul>
+</nav>
+<div class="main">
+    <img src="bg-main.jpg">
+</div>
+</body>
+</html>
