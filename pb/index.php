f81c6919362c10dac9262325415ee4cd852c82f5
Kpro KGB <pchelper13@gmail.com>
2015-10-27 07:57:11 +0200

    pb_index

--- pb/index.php
+++ pb/index.php
@@ -8,13 +8,13 @@
 <nav class="header">
     <img src="logo_big2.png">
     <ul>
-        <li><a href="">О ГРУППЕ</a></li>
-        <li><a href="">КАРЬЕРА</a></li>
-        <li><a href="">ВОЙТИ</a></li>
+        <li><a href="/about">О центре</a></li>
+        <li><a href="/carrier">Присоединится к команде</a></li>
+        <li><a href="/login">Войти</a></li>
     </ul>
 </nav>
 <div class="main">
-    <img src="bg-main.jpg">
+    <h1>Центр претензионно-исковой работы ПАТ КБ "ПриватБанк"</h1>
 </div>
 </body>
 </html>
