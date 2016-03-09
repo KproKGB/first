0665b83c59b463a997e692bc8bae3f862879907e
KproKGB <pchelper13@gmail.com>
2016-03-09 17:17:31 +0200

    Otest2

---
+++ O_test_2/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'lib/Database.php';
+require_once 'config/database.php';
+echo "Hello";
---
+++ O_test_2/lib/lessons.class.php
@@ -0,0 +1,8 @@
+abstract class Lessons {
+
+  public $duration; //dlitel'nost'
+  public $rate; //tarif
+
+  public function getPrice() {}
+
+}
